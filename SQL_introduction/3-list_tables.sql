09cce68133c0a22d18f235b3e5a9fa549b3c00de
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-16 16:59:36 +0100

    task 3 sorted

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,6 @@
 -- list of items in table called mysql
-SELECT table_name FROM information_schema.tables WHERE table_schema = 'mysql';
+-- SELECT table_name FROM information_schema.tables WHERE table_schema = 'mysql';
+USE mysql;
+
+
+SHOW tables;
