748bdab79538b9ee1f84f4998d0f8b7143fe398a
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-18 00:48:22 +0100

    task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- creating and granting certain permission
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
