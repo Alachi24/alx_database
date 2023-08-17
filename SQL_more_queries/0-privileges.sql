2bd1eb643d4e4db78d265b2be7e01f9a24b0fc98
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-17 23:28:11 +0100

    task 0 sorted

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
 -- listing set of permission granted to a user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
