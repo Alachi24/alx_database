678a9e8e68b45289a21a4c4eb97e6158acf3e167
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-08-17 23:35:28 +0100

    task 1 sorted

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creating a user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
-SET PASSWORD FOR 'user_0d_1'@'localhost'= "user_0d_1_pwd"
+SET PASSWORD FOR 'user_0d_1'@'localhost'= "user_0d_1_pwd";
